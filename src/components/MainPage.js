c5af1e4b03af521461b5507f7bb666d01f35ff8c
Tolisz <anatolii.radionov2002@gmail.com>
2023-01-13 01:44:46 +0100

    Layout

--- src/components/MainPage.js
+++ src/components/MainPage.js
@@ -1,19 +1,115 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+
+// microsoft
+import { protectedResources } from "../authConfig";
+import useFetchWithMsal from '../hooks/useFetchWithMsal';
+
+
+// myComponents
+import SortingOption from './SortingOption';
+
+// css 
+import "../styles/MainPage.css"
 
 export const MainPage = ( { isInquiries} ) => {
-
 	
+    const { execute, isLoading } = useFetchWithMsal({
+        scopes: protectedResources.apiLoanComparer.scopes.read,
+    });
+	
+	const [sortingParametrs, setSortingParametrs] = useState([]);
+	const [states, setStates] = useState([]);
+
+	useEffect(() => 
+	{
+		getSortingParametrs();
+		getStates();
+	}, [])
+
+	const getSortingParametrs = async () => 
+	{
+		execute("GET", protectedResources.apiLoanComparer.endpoint + '/api/ResponsesManagment/GetSortingParameters')
+		.then( (response) => 
+		{
+			setSortingParametrs(response);
+		})
+		.catch ( (e) => 
+		{
+			console.log("getSortingParametrs", e);
+		})
+	}
+
+	const getStates = async () => 
+	{
+		execute("GET", protectedResources.apiLoanComparer.endpoint + '/api/ResponsesManagment/GetResponsesStates')
+		.then( (response) => 
+		{
+			setStates(response);
+		})
+		.catch ( (e) => 
+		{
+			console.log("getStates", e);
+		})
+	}
 
 	return (
-		<div>
-			MainPage
+		<div className='MainPage-island'>
+			<div className='MainPage-whole'>				
+				<div className='MainPage-sortParametrs'>
+					<div className='MainPage-sortParametrs-element'>
+						<label> Parametr sortowania </label>
+						<select>
+							{ sortingParametrs ? sortingParametrs.map( (param, index) => 
+							(	
+								<SortingOption key={index} value={param} text={param} />
+							)) : null}
+						</select>
+					</div>
 
-			{ isInquiries 
-				? 
-				<div> Przypadek 1 </div>
-				: 
-				<div> Przypadek 2 </div>
-			}	
+					<div className='MainPage-sortParametrs-element'>
+						<label>State</label>
+						<select>
+							{ states ? states.map( (param, index) => 
+							(
+								<SortingOption key={index} value={param} text={param} />
+							) ) : null}
+						</select>
+					</div>
+					
+					<div className='MainPage-sortParametrs-element'>
+						<label>minAmount</label>
+						<input></input>
+					</div>
+					<div className='MainPage-sortParametrs-element'>
+						<label>maxAmount</label>
+						<input></input>
+					</div>
+					<div className='MainPage-sortParametrs-element'>
+						<label>minRate</label>
+						<input></input>
+					</div>
+					<div className='MainPage-sortParametrs-element'>
+						<label>maxRate</label>
+						<input></input>
+					</div>
+					<div className='MainPage-sortParametrs-element'>
+						<label>minIncomeLevel</label>
+						<input></input>
+					</div>
+					<div className='MainPage-sortParametrs-element'>
+						<label>maxIncomeLevel</label>
+						<input></input>
+					</div>
+					<div className='MainPage-sortParametrs-element'>
+						<label>PageSize</label>
+						<input></input>
+					</div>
+				</div>
+					
+			</div>
+			<div className='MainPage-whole-offers'>
+				Tu będą wyświetlane oferty 
+			</div>
 		</div>
 	)
 }
---
+++ src/components/SortingOption.js
@@ -0,0 +1,8 @@
+
+const SortingOption = ({value, text}) => {
+	return (
+		<option value={value}>{text}</option>
+	)
+}
+
+export default SortingOption
